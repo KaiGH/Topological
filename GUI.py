68e83f1e4b820c2af2de3520351f603d6fee3d42
BeckhamTup <43853473+BeckhamTup@users.noreply.github.com>
2020-03-10 13:40:29 +0000

    Update GUI.py
    - improved layout
    -added yaml selection

--- GUI.py
+++ GUI.py
@@ -7,120 +7,185 @@ import copy
 class GUI(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        self.yamlPath = ""
         self.mapPath = ""
         self.tmapPath = ""
 
+        #intial frame all elements are stored in
         container = tk.Frame(self)
         container.grid(row=0,column=0, sticky="nsew")
 
-        #container.grid_rowconfigure(0, weight=1)
-        #container.grid_columnconfigure(0, weight=1)
-
+        #array of frames
         self.frames = {}
+        #intialises start page
         self.frames["Start"] = StartPage(container, self)
+        #intialises edit page
         self.frames["Edit"] = EditingPage(container, self)
+        #builds start page
         self.frames["Start"].grid(row=0,column=0, sticky="nsew")
+        #builds edit page
         self.frames["Edit"].grid(row=0,column=0, sticky="nsew")
 
+        #calls show frame method to bring start page to front
         self.show_frame("Start")
 
+        #brings page to front
     def show_frame(self, page):
         frame = self.frames[page]
         frame.tkraise()
         frame.onShow()
 
+        #start page class
 class StartPage(tk.Frame):
      def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.mainGUI = controller
         #inserted text label
-        YAML=Label(self, width= 50, text ="Insert YAML File Path")
-        Map =Label(self, width= 50, text = "Insert Map File Path")
+        YAML =Label(self, width= 20, text = "Insert YAML File Path")
+        TMAP=Label(self, width= 20, text ="Insert TMAP File Path")
+        Map =Label(self, width= 20, text = "Insert Map File Path")
         #positioning of text label
         YAML.grid(row=3, column=0)
-        Map.grid(row=3, column=2)  
+        TMAP.grid(row=3, column=2)
+        Map.grid(row=3, column=4)  
         #declaring button
         YAMLButton = ttk.Button(self, text= "Open YAML File", command =self.YAML)
+        TMAPButton = ttk.Button(self, text= "Open TMAP File", command =self.TMAP)
         MapButton = ttk.Button(self, text= "Open Map File",  command =self.Map)
+
         nextbutton = ttk.Button(self, text= "Generate", command= lambda: controller.show_frame("Edit"))
         #postioning button
         YAMLButton.grid(row=3, column=1)
-        MapButton.grid(row=3, column=3)
-        nextbutton.grid(row= 3, column= 4)
+        TMAPButton.grid(row=3, column=3)
+        MapButton.grid(row=3, column=5)
+        nextbutton.grid(row= 3, column= 6)
 
-      
+     #selecting YAML file
+     def YAML(self):
+         #sets file type
+        self.yamlfilename = filedialog.askopenfilename(title = "Select File",filetypes =[('YAML Files', '*.yaml')])
+
+        #if user clicks on open file and then does not select path this will display
+        if(self.yamlfilename == ""):
+            self.yamlfilename = "/Insert Map File Path"
+        #sets variable for yaml path for use in file
+        self.mainGUI.yamlPath = self.yamlfilename
+        #displays chosen file path
+        YAMLP=Label(self, width= 20, text =self.mainGUI.yamlPath.split("/")[-1])
+        YAMLP.grid(row=3, column=0) 
+
      #selecting map file
      def Map(self):
-        self.filename = filedialog.askopenfilename(title = "Select A File",filetypes =[('PGM Files', '*.pgm')])
-        if(self.yamlfilename == ""):
-           self.yamlfilename = "/Insert Map File Path"
-        self.mainGUI.mapPath = self.filename
-        MapP=Label(self, width= 50, text =self.mainGUI.mapPath.split("/")[-1])
-        MapP.grid(row=3, column=2)
+         #sets file type
+        self.mapfilename = filedialog.askopenfilename(title = "Select File", filetypes =[('PGM Files', '*.pgm')])
+
+        #if user clicks on open file and then does not select path this will display
+        if(self.mapfilename == ""):
+           self.mapfilename = "/Insert Map File Path"
+
+        #sets variable for map path for use in file
+        self.mainGUI.mapPath = self.mapfilename
+        #displays chosen file path
+        MapP=Label(self, width= 20, text =self.mainGUI.mapPath.split("/")[-1])
+        MapP.grid(row=3, column=4)
         
-    #selecting node file
-     def YAML(self):
-        self.yamlfilename = filedialog.askopenfilename(title = "Select A File", filetypes =[('TMAP Files', '*.tmap')])
-        if(self.yamlfilename == ""):
-            self.yamlfilename = "/Insert YAML File Path"
-        self.mainGUI.tmapPath = self.yamlfilename
-        YAMLp=Label(self, width= 50, text = self.mainGUI.tmapPath.split("/")[-1])
-        YAMLp.grid(row=3, column=0)
+    #selecting tmap file
+     def TMAP(self):
+         #sets file type
+        self.tmapfilename = filedialog.askopenfilename(title = "Select A File", filetypes =[('TMAP Files', '*.tmap')])
+        #if user clicks on open file and then does not select path this will display
+        if(self.tmapfilename == ""):
+            self.tmapfilename = "/Insert TMAP File Path"
+
+        #sets variable for map path for use in file
+        self.mainGUI.tmapPath = self.tmapfilename
+        #displays chosen file path
+        TMAPp=Label(self, width= 20, text = self.mainGUI.tmapPath.split("/")[-1])
+        TMAPp.grid(row=3, column=2)
 
      def onShow(self):
          return
 
+#editing page class
 class EditingPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent) 
         self.mainGUI = controller
+
         self.header = Frame(self)
         self.header.grid(row=0, column = 1)
-        self.buttons = Frame(self)
-        self.buttons.grid(row = 1, column=0, sticky = W)
-        FilePagebutton = ttk.Button(self.header, text ="select different files", command= lambda:controller.show_frame("Start"))
+
+        self.footer = Frame(self)
+        self.footer.grid(row=3, column = 1)
+
+        self.buttonContainer = Frame(self)
+        self.buttonContainer.grid(row = 1, column=0)
+
+        self.nodeButtons = Frame(self.buttonContainer)
+        self.nodeButtons.grid(row = 1, column=0)
+
+        Blank1=Label(self.buttonContainer, width= 20,text ="Insert Parameters:")
+        Blank1.grid(row=2, column=0)
+
+        self.nodeButtonsEntry = Frame(self.buttonContainer)
+        self.nodeButtonsEntry.grid(row = 3, column=0)
+
+        self.pathButtons = Frame(self.buttonContainer)
+        self.pathButtons.grid(row = 6, column=0)
+
+        Blank2=Label(self.buttonContainer, width= 20,text ="")
+        Blank2.grid(row=5, column=0)
+
+        self.pathButtonsEntry = Frame(self.buttonContainer)
+        self.pathButtonsEntry.grid(row = 8, column=0)
+        
+        FilePagebutton = ttk.Button(self.header, width= 20, text ="select different files", command= lambda:controller.show_frame("Start"))
         FilePagebutton.grid()
+        Save = ttk.Button(self.footer, width= 20, text= "Save", command = self.mapSave)
+        Save.grid(row=1, column=1)
 
+        AddNode = ttk.Button(self.nodeButtons, width= 20, text= "Add New Node", command = self.addNode)
+        AddNode.grid(row=1, column=1)
 
-        X=Label(self.buttons, text ="Insert X Coordinate")
-        self.XEntry=Entry(self.buttons, width= 50, text ="Insert X Coordinate")
-        X.grid(row=1, column=0, sticky = W)
-        self.XEntry.grid(row=1, column=1, sticky = W)
+        DeleteNode = ttk.Button(self.nodeButtons, width= 20, text= "Delete A Node", command = self.nodeDelete)
+        DeleteNode.grid(row=1, column=2)
 
-        Y=Label(self.buttons, text ="Insert Y Coordinate")
-        self.YEntry=Entry(self.buttons, width= 50, text ="Insert Y Coordinate")
-        Y.grid(row=2, column=0, sticky = W)
-        self.YEntry.grid(row=2, column=1, sticky = W)
+        EditNode = ttk.Button(self.nodeButtons, width= 20, text= "Edit A Node" , command = self.nodeEdit)
+        EditNode.grid(row=1, column=3)
 
-        nodeName=Label(self.buttons, text ="Insert Node Name")
-        self.nodeNameEntry=Entry(self.buttons, width= 50, text ="Insert Node Name ")
-        nodeName.grid(row=3, column=0, sticky = W)
-        self.nodeNameEntry.grid(row=3, column=1, sticky = W)
+        nodeName=Label(self.nodeButtonsEntry, width= 30,text ="Insert Node Name")
+        self.nodeNameEntry=Entry(self.nodeButtonsEntry, width= 30, text ="Insert Node Name ")
+        nodeName.grid(row=2, column=1)
+        self.nodeNameEntry.grid(row=2, column=2)
 
-        AddNode = ttk.Button(self.buttons, text= "Add New Node", command = self.addNode)
-        AddNode.grid(row=4, column=1, sticky = W)
-        DeleteNode = ttk.Button(self.buttons, text= "Delete A Node", command = self.nodeDelete)
-        DeleteNode.grid(row=4, column=1)
-        EditNode = ttk.Button(self.buttons, text= "Edit A Node" , command = self.nodeEdit)
-        EditNode.grid(row=4, column=1, sticky = E)
+        X=Label(self.nodeButtonsEntry, width= 30,text ="Insert X Coordinate")
+        self.XEntry=Entry(self.nodeButtonsEntry, width= 30, text ="Insert X Coordinate")
+        X.grid(row=3, column=1,)
+        self.XEntry.grid(row=3, column=2)
 
-        nodeName1=Label(self.buttons, text ="Insert Starting Node Name")
-        self.nodeName1Entry=Entry(self.buttons, width= 50, text ="Insert Starting Node Name ")
-        nodeName1.grid(row=5, column=0, sticky = W)
-        self.nodeName1Entry.grid(row=5, column=1, sticky = W)
+        Y=Label(self.nodeButtonsEntry, width= 30,text ="Insert Y Coordinate")
+        self.YEntry=Entry(self.nodeButtonsEntry, width= 30, text ="Insert Y Coordinate")
+        Y.grid(row=4, column=1)
+        self.YEntry.grid(row=4, column=2)
 
-        nodeName2=Label(self.buttons, text ="Insert Destination Node Name")
-        self.nodeName2Entry=Entry(self.buttons, width= 50, text ="Insert Destination Node Name ")
-        nodeName2.grid(row=6, column=0, sticky = W)
-        self.nodeName2Entry.grid(row=6, column=1, sticky = W)
+        AddPath = ttk.Button(self.pathButtons, width= 20, text= "Add Path" , command = self.pathAdd)
+        AddPath.grid(row=1, column=1)
 
-        AddPath = ttk.Button(self.buttons, text= "Add Path" , command = self.pathAdd)
-        AddPath.grid(row=7, column=1, sticky = W, padx=40)
+        DeletePath = ttk.Button(self.pathButtons, width= 20, text= "Delete Path" , command = self.pathDelete)
+        DeletePath.grid(row=1, column=2)
 
-        DeletePath = ttk.Button(self.buttons, text= "Delete Path" , command = self.pathDelete)
-        DeletePath.grid(row=7, column=1, sticky = E, padx=40)
-        save = ttk.Button(self.buttons, text= "Save", command = self.mapSave)
-        save.grid(row=8, column=1)
+        Blank3=Label(self.buttonContainer, width= 20,text ="Insert Parameters:")
+        Blank3.grid(row=7, column=0)
+
+        nodeName1=Label(self.pathButtonsEntry, width= 30,text ="Insert Starting Node Name")
+        self.nodeName1Entry=Entry(self.pathButtonsEntry, width= 30, text ="Insert Starting Node Name ")
+        nodeName1.grid(row=2, column=1)
+        self.nodeName1Entry.grid(row=2, column=2)
+
+        nodeName2=Label(self.pathButtonsEntry, width= 30,text ="Insert Destination Node Name")
+        self.nodeName2Entry=Entry(self.pathButtonsEntry, width= 30, text ="Insert Destination Node Name ")
+        nodeName2.grid(row=3, column=1)
+        self.nodeName2Entry.grid(row=3, column=2)
 
 
 
@@ -133,7 +198,6 @@ class EditingPage(tk.Frame):
     def nodeDelete(self):
         #nodeList = self.readYaml(self.mainGUI.tmapPath)
         nodeName = self.nodeNameEntry.get()
-        print(self.nodeNameEntry.get())
         #Searches from the First to the Last Node
         for index in range(len(self.nodeList)):      
         #If the Node Matches with the Inputted Node
@@ -200,11 +264,11 @@ class EditingPage(tk.Frame):
         #returns the next number in sequence
         return numbers[-1] + 1
 
-    def nodeCoords(self, filename):
+    def nodeCoords(self):
         #returns all nodes that exist in file
         return [[node["node"]["pose"]["position"]["x"],node["node"]["pose"]["position"]["y"], node["meta"]["node"]]for node in self.nodeList]
 
-    def pathNums(self,filename):
+    def pathNums(self):
         #nodeList = self.readYaml(filename)
         paths = []
         for i in range(len(self.nodeList)):     
@@ -250,38 +314,42 @@ class EditingPage(tk.Frame):
         stream.close()
 
     def drawingWayPoints(self):
-        points = self.nodeCoords(self.mainGUI.tmapPath)
-        paths = self.pathNums(self.mainGUI.tmapPath)
+        points = self.nodeCoords()
+        paths = self.pathNums()
 
         for path in paths: 
+            #nodes that connect
             pointA = path[0][0]
             pointB = path[1][0]
-    
+            #variables for node x, y 
             pointACoords = None
             pointBCoords = None
+
             for point in points:
                 if point[2] == pointA:
                     pointACoords = [point[0], point[1]]
                 if point[2] == pointB:
                     pointBCoords = [point[0], point[1]]
             if(pointACoords != None and pointBCoords != None):
-                self.draw_line(pointACoords, pointBCoords, 0.05)
+                self.draw_line(pointACoords, pointBCoords, self.yamlFile["resolution"])
         for point in points:
             #print(F"Plotting point {point[2][8:]}")
-            self.draw_point(point[0], point[1], 0.05, point[2][8:])
+            if(point[0] != "" or point[1] != ""):
+                self.draw_point(float(point[0]), float(point[1]), point[2][8:], float(self.yamlFile["resolution"]))
+#sticky = E, padx=40
 
     def loadImage(self):
         im = PILImage.open(self.mainGUI.mapPath)
         self.SCALE = 3
-        self.ORIGIN = (-31.45, -12.45)
+        self.ORIGIN = (self.yamlFile["origin"][0], self.yamlFile["origin"][1])
         self.width, self.height = im.size
         self.canvas = Canvas(self, width=self.width/self.SCALE, height = self.height/self.SCALE)
-        self.canvas.grid(row = 1, column=1,sticky = E, padx=40)  
+        self.canvas.grid(row = 1, column=1)  
         image2=im.resize((int(self.width/self.SCALE),int(self.height/self.SCALE)),PILImage.ANTIALIAS)
         picture=ImageTk.PhotoImage(image2)
         label = Label(image=picture)
         label.image = picture # keep a reference!
-        label.grid(row = 1, column=1, sticky = E, padx=40)
+        label.grid(row = 1, column=1)
         label.grid_forget()
         self.canvas.create_image(0,0, anchor=NW, image=picture) 
         self.drawingWayPoints()
@@ -291,9 +359,8 @@ class EditingPage(tk.Frame):
         newBX, newBY =  (((float(b[0])-float(self.ORIGIN[0]))/scale)/self.SCALE), (self.height/self.SCALE)- (((float(b[1])-float(self.ORIGIN[1]))/scale)/self.SCALE)
         self.canvas.create_line(newAX, newAY, newBX, newBY)
 
-    def draw_point(self, x, y, scale, label):
-        x = float(x)
-        y = float(y)
+    def draw_point(self, x, y, label, scale):
+
         newX, newY =  (((x-self.ORIGIN[0])/scale)/self.SCALE), (self.height/self.SCALE)- (((y-self.ORIGIN[1])/scale)/self.SCALE)
         radius = 7.5
         self.canvas.create_oval(newX - radius, newY - radius, newX + radius, newY + radius, fill = 'blue')
@@ -303,6 +370,7 @@ class EditingPage(tk.Frame):
 
     def onShow(self):
         self.nodeList = self.readYaml(self.mainGUI.tmapPath)
+        self.yamlFile = self.readYaml(self.mainGUI.yamlPath)
         self.loadImage()
 
 
